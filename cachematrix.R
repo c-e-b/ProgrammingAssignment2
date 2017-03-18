931e14736067c5a4abe714b374503c1905211eb6
c-e-b <carlebbinghaus@gmail.com>
2017-03-18 15:11:22 +0100

    filled cachematrix.R with mean-example and swapped mean with inverse

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        matrixinverse <- NULL
+        set <- function(y) {
+                x <<- y
+                matrixinverse <<- NULL
+        }
+        get <- function() x
+        setmatrixinverse <- function(inverse) matrixinverse <<- mean
+        getmatrixinverse <- function() matrixinverse
+        list(set = set,
+             get = get,
+             setmatrixinverse = setmatrixinverse,
+             getmatrixinverse = getmatrixinverse)
 }
 
 
