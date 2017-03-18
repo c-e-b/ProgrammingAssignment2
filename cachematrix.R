78e7b3e44e785f61e0c3884174af355534ea6543
c-e-b <carlebbinghaus@gmail.com>
2017-03-18 15:52:01 +0100

    working (tested with a few matrices) and filled with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Requirements: "For this assignment, assume that the matrix supplied is always invertible."
+## solve(X) doesn't work if it's not invertible - I guess
+## 
 
-## Write a short comment describing this function
+## The first function creates a special "matrix" object that can cache its inverse.
+## Like in the given example makeVector it creates a list contatining a function to
+## 1. set the value of the matrix, 2. get the value of the matrix, 3. set the value of the inverse, 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         matrixinverse <- NULL
@@ -10,7 +13,7 @@ makeCacheMatrix <- function(x = matrix()) {
                 matrixinverse <<- NULL
         }
         get <- function() x
-        setmatrixinverse <- function(inverse) matrixinverse <<- mean
+        setmatrixinverse <- function(inverse) matrixinverse <<- inverse
         getmatrixinverse <- function() matrixinverse
         list(set = set,
              get = get,
@@ -18,9 +21,40 @@ makeCacheMatrix <- function(x = matrix()) {
              getmatrixinverse = getmatrixinverse)
 }
 
-
-## Write a short comment describing this function
+## The second function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then the cacheSolve function
+## should retrieve the inverse from the cache.
+## In the output you can see the printed text "getting cached data" if it works.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        matrixinverse <- x$getmatrixinverse()
+        if(!is.null(matrixinverse)) {
+                message("getting cached data")
+                return(matrixinverse)
+        }
+        data <- x$get()
+        matrixinverse <- solve(data, ...) ## here the inverse function solve(X) really generates the inverse
+        x$setmatrixinverse(matrixinverse)
+        matrixinverse
+        ## now the function returns a matrix that is the inverse of 'x'
 }
+
+#### example input and output for using/testing this R-script
+#### @self: don't forget to run the functions first
+#### this whole example part is more for myself to remember in a few months than the peer who grades my script
+### read it and use the inputs (lines with > in front of them) to try it if you like :)
+## > x <- rbind(c(4, 6), c(4, -1))
+## > testmatrix <- makeCacheMatrix(x)
+## > testmatrix$get()
+## [,1] [,2]
+## [1,]    4    6
+## [2,]    4   -1
+## > cacheSolve(testmatrix)
+## [,1]       [,2]
+## [1,] 0.03571429  0.2142857
+## [2,] 0.14285714 -0.1428571
+## > cacheSolve(testmatrix)
+## getting cached data      ## this is the printed message that only comes if matrixinverse is just returned instead of computed freshly
+## [,1]       [,2]
+## [1,] 0.03571429  0.2142857
+## [2,] 0.14285714 -0.1428571
